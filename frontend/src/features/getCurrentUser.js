c17cf42ee6748625924d8922c8d23f459ae87654
Arjun <neeneeraj04@gmail.com>
2026-09-24 00:43:11 +0530

    Updated backend

--- frontend/src/features/getCurrentUser.js
+++ frontend/src/features/getCurrentUser.js
@@ -1,13 +1,13 @@
 import api from "../../utils/axios.js";
 
 const getCurrentUser = async () => {
-    try {
-        const { data } = await api.get("/api/auth/me");
-        return data;
-    } catch (e) {
-        console.log(e.response?.data || e.message);
-        return null;
-    }
+  try {
+    const { data } = await api.get("/api/me");
+    return data;
+  } catch (e) {
+    console.log(e.response?.data || e.message);
+    return null;
+  }
 };
 
 export default getCurrentUser;
